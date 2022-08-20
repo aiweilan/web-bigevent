2a506a5ba0d3bb6eb7a622d6b5e388f3720396c3
aiweilan <1612319840@qq.com>
2022-08-20 20:55:52 +0800

    完成了主页功能开发

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+$(function () {
+    // 获取用户信息
+    getUserInfo();
+    // 点击退出实现退出功能
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出吗?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg('获取用户信息失败')
+            }
+            renderavatar(res.data);
+        },
+        complete: function (res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+
+        }
+    })
+}
+// 渲染用户信息
+function renderavatar(user) {
+    // 获取用户名称
+    var name = user.nickname || user.username
+    // 设置欢迎文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 按需渲染用户头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide();
+    }
+    else {
+        $('.layui-nav-img').hide()
+        // 第一位名称转大写toUpperCase()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
